87afb2877d7ead7458fd528af97c0f174b6520d2
ashokguntuku <ashokguntuku@gmail.com>
2018-02-21 15:58:33 +0530

    first version

---
+++ src/test/java/asokNLPractice/AppiumTest.java
@@ -0,0 +1,20 @@
+package asokNLPractice;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid()	
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void IosApps()	
+	{
+		System.out.println("IosApps");
+	}
+	
+
+}
---
+++ src/test/java/asokNLPractice/SeleniumTest.java
@@ -0,0 +1,24 @@
+package asokNLPractice;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void helloWorld()	{
+		System.out.println("HelloWorld!!");
+	}
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+
+}
